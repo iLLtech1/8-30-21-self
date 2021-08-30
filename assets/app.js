57cb4876f90041252b19b4d23ce162d8795d5a9f
Chris Livingston <clivingston@gmail.com>
2021-08-30 14:54:14 -0700

    adding code

---
+++ assets/app.js
@@ -0,0 +1,53 @@
+// const adder = (a, b) => {
+//   return a + b
+// }
+
+// console.log(adder(5, 7))
+
+// const subtr = (a, b) => {
+//   return a - b 
+// }
+
+// console.log(subtr(10, 5))
+
+// const multi = (a, b) => {
+//   return a * b 
+// }
+
+// console.log(multi(5, 10))
+
+// const pow = (a, b) => {
+//   return a ** b
+// }
+
+// console.log(pow(50, 2))
+
+// console.log(typeof(pow))
+
+// pow is not a var but a function because of =>
+
+console.log(process.argv)
+
+
+
+const movies = require('./stuff') 
+
+console.log(movies)
+
+// const songs = require('./stuff')
+
+// console.log(songs)
+
+// const foods = require('./stuff')
+
+// console.log(foods)
+
+// const games = require('./stuff')
+
+// console.log(games)
+
+// const fs = require('fs')
+
+// fs.writeFile('some.txt', 'some other text', err => {
+//   if (err) { console.log(err) }
+// })
---
+++ assets/stuff.js
@@ -0,0 +1,15 @@
+const movies =['Pirates' , 'Kong']
+
+module.exports = movies
+
+// const songs = ['song1', 'song2', 'song3', 'song4', 'song5']
+
+// module.exports = songs
+
+// const foods = ['food1', 'food2', 'food3', 'food4', 'food5']
+
+// module.exports = foods
+
+// const games = ['game1', 'game2', 'game3', 'game4', 'game5']
+
+// module.exports = games
