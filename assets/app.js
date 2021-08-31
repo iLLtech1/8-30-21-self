d1464bdcabd18f334863d416ee7a0a38225cd73e
Chris Livingston <clivingston@gmail.com>
2021-08-31 10:00:17 -0700

    arithmatic nodejs

--- assets/app.js
+++ assets/app.js
@@ -26,13 +26,13 @@
 
 // pow is not a var but a function because of =>
 
-console.log(process.argv)
+// console.log(process.argv)
 
 
 
-const movies = require('./stuff') 
+// const movies = require('./stuff') 
 
-console.log(movies)
+// console.log(movies)
 
 // const songs = require('./stuff')
 
@@ -51,3 +51,42 @@ console.log(movies)
 // fs.writeFile('some.txt', 'some other text', err => {
 //   if (err) { console.log(err) }
 // })
+
+const adder = (a, b) => {
+  return a + b
+}
+const subtracter = (a, b) => {
+  return a - b
+}
+const multiplier = (a, b) => {
+  return a * b
+}
+const divider = (a, b) => {
+  return a / b
+}
+const exponent = (a, b) => {
+  return a ** b
+}
+
+let [, , operation, num1, num2] = process.argv
+
+num1 = parseInt(num1)
+num2 = parseInt(num2)
+
+switch (operation) {
+  case 'add':
+    console.log(adder(num1, num2))
+    break
+  case 'subtract':
+    console.log(subtracter(num1, num2))
+    break
+  case 'multiply':
+    console.log(multiplier(num1, num2))
+    break
+  case 'divide':
+    console.log(divider(num1, num2))
+    break
+  case 'exponent':
+    console.log(exponent(num1, num2))
+    break
+}
